8f2b4565217293306c722e6d33a18fc9d0bf5a92
shin04 <daikon0413@gmail.com>
2021-07-02 21:06:15 +0900

    remove unecessary args from audioset.py

--- src/datasets/audioset.py
+++ src/datasets/audioset.py
@@ -8,7 +8,6 @@ import soundfile as sf
 from torch.utils.data import Dataset
 import h5py
 
-from utils.audio_checker import get_audio_names
 from datasets.utils import random_crop, mel_spec
 
 
@@ -17,33 +16,6 @@ class DataType(Enum):
     SPECTROGRAM = 'spectrogram'
     LOGMEL = 'logmel'
     MFCC = 'mfcc'
-
-
-class AudioSetDataset(Dataset):
-    def __init__(self, metadata_path: Path, sr: int = 32000, crop_sec: int = None):
-        meta_df = pd.read_csv(metadata_path, header=None)
-
-        self.audio_pathes = meta_df[0].values.tolist()
-        self.sr = sr
-        if crop_sec is None:
-            self.crop_size = None
-        else:
-            self.crop_size = crop_sec * sr
-
-    def __len__(self):
-        return len(self.audio_pathes)
-
-    def __getitem__(self, idx: int):
-        audio_path = self.audio_pathes[idx]
-
-        wave_data, _ = sf.read(audio_path)
-
-        if self.crop_size is not None:
-            wave_data, _ = random_crop(wave_data, self.crop_size)
-
-        wave_data = wave_data.reshape((1, -1))
-
-        return np.float32(wave_data)
 
 
 class HDF5Dataset(Dataset):
@@ -101,25 +73,44 @@ class HDF5Dataset(Dataset):
         return np.float32(waveform)
 
 
+class AudioSetDataset(Dataset):
+    def __init__(self, metadata_path: Path, sr: int = 32000, crop_sec: int = None):
+        meta_df = pd.read_csv(metadata_path, header=None)
+
+        self.audio_pathes = meta_df[0].values.tolist()
+        self.sr = sr
+        if crop_sec is None:
+            self.crop_size = None
+        else:
+            self.crop_size = crop_sec * sr
+
+    def __len__(self):
+        return len(self.audio_pathes)
+
+    def __getitem__(self, idx: int):
+        audio_path = self.audio_pathes[idx]
+
+        wave_data, _ = sf.read(audio_path)
+
+        if self.crop_size is not None:
+            wave_data, _ = random_crop(wave_data, self.crop_size)
+
+        wave_data = wave_data.reshape((1, -1))
+
+        return np.float32(wave_data)
+
+
 class CLDataset(Dataset):
     def __init__(
-        self, audio_path: str, metadata_path: str = None,
+        self, metadata_path: str,
         q_type: DataType = 'raw',
         k_type: DataType = 'raw',
         crop_sec=3,
         n_mels: int = 80,
         freq_shift_size: int = None
     ):
-        self.audio_path = Path(audio_path)
-
-        if metadata_path is None:
-            self.audio_names = get_audio_names(audio_path, crop_sec)
-        else:
-            df = pd.read_csv(Path(metadata_path), header=None)
-            self.audio_names = df[0].values.tolist()
-
-        # df = pd.read_csv(Path(metadata_path), header=None)
-        # self.audio_names = df[0].values.tolist()
+        df = pd.read_csv(Path(metadata_path), header=None)
+        self.audio_names = df[0].values.tolist()
 
         self.q_type = q_type
         self.k_type = k_type
@@ -153,7 +144,6 @@ class CLDataset(Dataset):
 
 if __name__ == '__main__':
     dataset = CLDataset(
-        audio_path='/ml/dataset/audioset/audio/balanced_train_segments',
         metadata_path='/ml/meta/meta_train.csv',
         freq_shift_size=20
     )
