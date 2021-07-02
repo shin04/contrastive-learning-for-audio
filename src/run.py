2e03be01e6ab49c17df9657731c12a356c662fd0
shin04 <daikon0413@gmail.com>
2021-07-02 21:01:43 +0900

    rename train.py > run.py and make loss.py

--- src/run.py
+++ src/run.py
@@ -10,33 +10,10 @@ from torch.utils.tensorboard import SummaryWriter
 
 from datasets.audioset import CLDataset, AudioSetDataset, HDF5Dataset
 from models.cl_model import CLModel
-from utils.cosine_decay_rule import CosineDecayScheduler
+from utils.scheduler import CosineDecayScheduler
+from utils.losses import nt_xent_loss
 
 TIME_TEMPLATE = '%Y%m%d%H%M%S'
-
-
-def nt_xent_loss(q, pos_k, temperature):
-    # reference
-    # https://www.youtube.com/watch?v=_1eKr4rbgRI
-    # https://colab.research.google.com/drive/1UK8BD3xvTpuSj75blz8hThfXksh19YbA?usp=sharing#scrollTo=GBNm6bbDT9J3
-
-    out = torch.cat([q, pos_k], dim=0)
-    n_samples = len(out)
-
-    # Full similarity matrix
-    cov = torch.mm(out, out.t().contiguous())
-    sim = torch.exp(cov / temperature)
-
-    mask = ~torch.eye(n_samples, device=sim.device).bool()
-    neg = sim.masked_select(mask).view(n_samples, -1).sum(dim=-1)
-
-    # Positive similarity
-    pos = torch.exp(torch.sum(q * pos_k, dim=-1) / temperature)
-    pos = torch.cat([pos, pos], dim=0)
-
-    loss = -torch.log(pos / neg).mean()
-
-    return loss
 
 
 @ hydra.main(config_path='../config', config_name='pretrain')
