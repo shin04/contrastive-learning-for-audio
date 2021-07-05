fa79482f61104450904b3a8c5a9d76afdb3821ca
shin04 <daikon0413@gmail.com>
2021-07-05 14:19:16 +0900

    fix esc_run

--- src/esc_train.py
+++ src/esc_train.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from pathlib import Path
+import os
 
 import hydra
 
@@ -18,7 +19,7 @@ from models.raw_model import Conv160
 TIME_TEMPLATE = '%Y%m%d%H%M%S'
 
 
-def train(trainloader, optimizer, device, global_step,  model, criterion, writer, fold):
+def train(trainloader, optimizer, device, global_step,  model, criterion, fold, writer=None):
     model.train()
 
     n_batch = len(trainloader)
@@ -44,13 +45,14 @@ def train(trainloader, optimizer, device, global_step,  model, criterion, writer
         correct = (predict == labels).sum()
         train_acc += correct.item()
 
-        writer.add_scalar(f"{fold}/loss", loss.item(), global_step)
-        writer.add_scalar(f"{fold}/acc", correct.item() /
-                          labels.size(0), global_step)
+        if writer is not None:
+            writer.add_scalar(f"{fold}/loss", loss.item(), global_step)
+            writer.add_scalar(f"{fold}/acc", correct.item() /
+                              labels.size(0), global_step)
         print(
             f'batch: {batch_num}/{n_batch}, '
-            f'loss: {loss.item()}, train loss: {train_loss/(batch_num+1)}, '
-            f'acc: {correct.item()/labels.size(0)}, train acc: {train_acc/total} ')
+            f'loss: {loss.item(): .6f}, train loss: {train_loss/(batch_num+1): .6f}, '
+            f'acc: {correct.item()/labels.size(0): .6f}, train acc: {train_acc/total: .6f} ')
         global_step += 1
 
     train_loss /= n_batch
@@ -84,7 +86,7 @@ def valid(validloader, device, model, criterion):
         valid_loss /= len(validloader)
         valid_acc /= total
 
-    print(f'val loss: {valid_loss}, val acc: {valid_acc}')
+    print(f'val loss: {valid_loss: .6f}, val acc: {valid_acc: .6f}')
 
 
 def test(testloader, device, model):
@@ -107,46 +109,67 @@ def test(testloader, device, model):
 
         valid_acc /= total
 
-    print(f'test acc: {valid_acc}')
+    print(f'test acc: {valid_acc: .6f}')
 
 
 @ hydra.main(config_path='/ml/config', config_name='esc')
 def run(cfg):
     """set config"""
+    debug = cfg['debug']
     path_cfg = cfg['path']
-    # preprocess_cfg = cfg['preprocess']
+    preprocess_cfg = cfg['preprocess']
     train_cfg = cfg['training']
 
-    """set path"""
+    """set pathes"""
     ts = datetime.now().strftime(TIME_TEMPLATE)
-    print(f"TIMESTAMP: {ts}")
+    print("TIMESTAMP:", ts)
+    print('DEBUG MODE:', debug)
 
     audio_path = Path(path_cfg['audio'])
     meta_path = Path(path_cfg['meta'])
     pretrain_model_path = Path(path_cfg['pretrain_model'])
 
     log_path = Path(path_cfg['tensorboard']) / ts
-    if not log_path.exists():
+    if (not debug) and (not log_path.exists()):
         log_path.mkdir(parents=True)
 
     print('PATH')
-    print(f'audio: {audio_path}')
-    print(f'meta: {meta_path}')
-    print(f'tensorboard: {log_path}')
+    print('audio:', audio_path)
+    print('meta:', meta_path)
+    if not debug:
+        print(f'tensorboard: {log_path}')
 
     """set parameters"""
     device = torch.device(cfg['device'])
+    num_worker = train_cfg['num_worker']
+    if num_worker == -1:
+        num_worker = os.cpu_count()
+
     n_epoch = train_cfg['n_epoch']
     batch_size = train_cfg['batch_size']
     lr = train_cfg['lr']
 
-    writer = SummaryWriter(log_dir=log_path)
+    sr = preprocess_cfg['sr']
+    crop_sec = preprocess_cfg['crop_sec']
+    n_mels = preprocess_cfg['n_mels']
+    freq_shift_size = preprocess_cfg['freq_shift_size']
 
     print('PARAMETERS')
-    print(f'device: {device}')
-    print(f'n_epoch: {n_epoch}')
-    print(f'batch_size: {batch_size}')
-    print(f'lr: {lr}')
+    print("device:", device)
+    print("num_worker:", num_worker)
+    print("n_epoch:", n_epoch)
+    print("batch_size:", batch_size)
+    print("lr:", lr)
+    print("sr", sr)
+    print("crop secconds", crop_sec)
+    print("n_mels:", n_mels)
+    print("frequency shift size:", freq_shift_size)
+
+    """tensorboard"""
+    if not debug:
+        writer = SummaryWriter(log_dir=log_path)
+    else:
+        writer = None
 
     """
     dividing data, reference below
@@ -173,7 +196,7 @@ def run(cfg):
             metadata_path=meta_path,
             folds=fold_dict['train'],
             data_type='raw',
-            data_crop_size=3
+            data_crop_size=crop_sec
         )
 
         validset = ESCDataset(
@@ -181,7 +204,7 @@ def run(cfg):
             metadata_path=meta_path,
             folds=fold_dict['valid'],
             data_type='raw',
-            data_crop_size=3
+            data_crop_size=crop_sec
         )
 
         testset = ESCDataset(
@@ -189,18 +212,18 @@ def run(cfg):
             metadata_path=meta_path,
             folds=fold_dict['test'],
             data_type='raw',
-            data_crop_size=3
+            data_crop_size=crop_sec
         )
 
         trainloader = DataLoader(
-            trainset, batch_size=batch_size, shuffle=True, pin_memory=True)
+            trainset, batch_size=batch_size, shuffle=True, num_workers=num_worker, pin_memory=True)
         validloader = DataLoader(
-            validset, batch_size=batch_size, shuffle=True, pin_memory=True)
+            validset, batch_size=batch_size, shuffle=True, num_workers=num_worker, pin_memory=True)
         testloader = DataLoader(
-            testset, batch_size=batch_size, shuffle=False, pin_memory=True)
+            testset, batch_size=batch_size, shuffle=False, num_workers=num_worker, pin_memory=True)
 
         """prepare model"""
-        cl_model = CLModel()
+        cl_model = CLModel(cfg=preprocess_cfg, is_preprocess=True).to(device)
         cl_model.load_state_dict(torch.load(pretrain_model_path))
         state_dict_keys = list(cl_model.state_dict().keys())
 
@@ -213,10 +236,10 @@ def run(cfg):
             elif 'spec_model' == _k[0]:
                 spec_model_dict[".".join(_k[1:])] = cl_model.state_dict()[k]
 
-        raw_model = Conv160().cuda()
+        raw_model = Conv160().to(device)
         raw_model.load_state_dict(raw_model_dict)
 
-        model = ESC_Model(raw_model, 422912, 512, 50).cuda()
+        model = ESC_Model(raw_model, 422912, 512, 50).to(device)
 
         # spec_model = CNN6()
         # spec_model.load_state_dict(spec_model_dict)
@@ -231,17 +254,19 @@ def run(cfg):
         train_global_step = 0
         for epoch in range(n_epoch):
             train_global_step, train_loss, train_acc = train(
-                trainloader, optimizer, device, train_global_step, model, criterion, writer, k_fold)
+                trainloader, optimizer, device, train_global_step, model, criterion, k_fold, writer)
             valid(validloader, device, model, criterion)
 
-            writer.add_scalar(f"{k_fold}/loss/epoch", train_loss, epoch)
-            writer.add_scalar(f"{k_fold}/acc/epoch", train_acc, epoch)
+            if not debug:
+                writer.add_scalar(f"{k_fold}/loss/epoch", train_loss, epoch)
+                writer.add_scalar(f"{k_fold}/acc/epoch", train_acc, epoch)
 
             print(
-                f'epoch: {epoch}/{n_epoch}, train loss: {train_loss}, train acc: {train_acc}')
+                f'epoch: {epoch}/{n_epoch}, train loss: {train_loss: .6f}, train acc: {train_acc: .6f}')
         test(testloader, device, model)
 
-    writer.close()
+    if not debug:
+        writer.close()
 
 
 if __name__ == '__main__':
